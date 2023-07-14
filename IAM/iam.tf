04aa8828701e864cb999d6efe99cf7dac76d25ab
skalajonson <129772543+skalajonson@users.noreply.github.com>
2023-07-14 21:31:50 +0300

    Update iam.tf

--- IAM/iam.tf
+++ IAM/iam.tf
@@ -1,9 +1,6 @@
 provider "aws" {
       region     = "eu-north-1"
-      access_key = "AKIAQGL7X2SFETNE3SZ6"
-      secret_key = "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"
-      #access_key = "AKIAQGL7X2SFPAF5ROUO"
-      #secret_key = "cYMewsFwpTo9EYB1PwsslpKhUcJ8yEsBF8z3GPD4"
+      
 }
 
 resource "aws_iam_user" "example_user" {
